ecd2becf6f58f07da599cfd3bdd3691d7d17bd3d
ViaAppSupport <117312902+ViaAppSupport@users.noreply.github.com>
2023-06-13 05:14:09 +0300

    ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -3,12 +3,12 @@ const { EmbedBuilder } = require('discord.js')
 module.exports.execute = async(client, interaction) => {
     
     const embed = new EmbedBuilder()
-.setDescription(`PreAI = \`${client.ws.ping}\` `)
+.setDescription(`Welcomer Bot = \`${client.ws.ping}\` `)
 interaction.reply({embeds: [embed] ,ephemeral:true})
 },
 
 module.exports.config = {
     name: "ping",
-    description: "Bot'un gecikmesini gösterir.",
+    description: "Shows the bot delay.",
     options: []
 }
